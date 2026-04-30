5d846a2a9ab2ed0a60164db7c316c8a328d2758b
dxhw <dhsiaowecks@gmail.com>
2026-04-30 14:56:33 -0400

    add comment contstraints

--- z3-model/validator.py
+++ z3-model/validator.py
@@ -9,3 +9,46 @@
 
 import json
 
+# pull in the JSONS
+
+# get user class year
+
+# get user data (demo with JSON)
+
+# get which constraints user cares about (default all TRUE dictionary)
+
+# Constraints:
+#   OLD:
+#       SCB:
+            # intro sequence (2 courses)
+            # intermediate (5 courses) - 1 from each category
+            # 2 pathways
+            # additional 1000+ (not in pathways)
+            # three additional courses
+            # humanities requirement (4)
+            # 1970 repetition requirement
+            # capstone (in pathway)
+        # AB:
+            # intro sequence (2 courses)
+            # intermediate (3 courses) - 2 categories+
+            # 1 pathways
+            # additional 1000+ (not in pathway)
+            # one additional course
+            # humanities requirement (2)
+            # 1970 repetition requirement
+    # New:
+    #     SCB:
+    #         intro sequence (2 courses)
+    #         math foundations (22)
+    #         3 foundations (AI, systems, theory)
+    #         5 1000+ levels - not artsy, not 1970
+    #         4 electives - basically anything (2 1970, linear, swe, 3 non-department)
+    #         capstone
+        # AB: 
+    #         intro sequence (2 courses)
+    #         math foundations (22)
+    #         3 foundations (AI, systems, theory)
+    #         2 1000+ levels - not artsy, not 1970
+    #         2 electives - basically anything (2 1970, linear, swe, 3 non-department)
+    #         capstone
+
